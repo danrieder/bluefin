f8835d45211a2f4ebc21238fb4ad6df245d6be82
Daniel Rieder <dan.rieder@protonmail.com>
2022-08-05 14:32:29 -0700

    a bit more before a rest

--- cs50/pset2/readability/readability.c
+++ cs50/pset2/readability/readability.c
@@ -1,18 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 /*
 int count_l(string);
 int count_w(string);
 int count_s(string);
-
-float l, w, s;
 */
 
-int main(void) 
-{
+float l, w, s;
+string txt;
+int count_letters(string text);
+
+int main(void) {
   string txt = get_string("Text: ");
-  //int i = strlen(txt);
-  printf("%s\n", txt);
+  int i = strlen(txt);
+  printf("%d letters\n", i);
 }
 
+int count_letters(string text) {
+  l = 0;
+  for (int i = 0, n = strlen(txt); i < n; i++)
+    l++;
+  //return l;
+}
