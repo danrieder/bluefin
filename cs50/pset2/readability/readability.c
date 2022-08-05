e1c096325d42e2602c97324e4e477db2597a24b3
Daniel Rieder <dan.rieder@protonmail.com>
2022-08-05 12:19:02 -0700

    initial commit

---
+++ cs50/pset2/readability/readability.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+/*
+int count_l(string);
+int count_w(string);
+int count_s(string);
+
+float l, w, s;
+*/
+
+int main(void) 
+{
+  string txt = get_string("Text: ");
+  //int i = strlen(txt);
+  printf("%s\n", txt);
+}
+
