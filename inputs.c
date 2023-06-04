2079f70bed56a91704adca057f492de14e36a6d5
Daniel Rieder <dan.rieder@protonmail.com>
2023-06-04 09:34:49 -0700

    ADD: some comments

--- inputs.c
+++ inputs.c
@@ -1,20 +1,27 @@
 # include <stdio.h>
 # include <string.h>
 
-int main()
+/* Get some user input 
+ * make sure it can have white space
+ * print what we got */
+
+int main(void)
 {
+    // Initialize the variables we need
     char name[25];  //bytes
     int age;
 
+    // Get the user input - deal with spaces in name
     printf("\nWhat's your name?");
     fgets(name, 25, stdin);
     name[strlen(name)-1] = '\0';
 
+    // Get the age
     printf("\nHow old are you?");
     scanf("%d", &age);
-
+  
+    // do something with what we got
     printf("Hello %s, how are you?\n", name);
     printf("You are %d years old\n", age);
 
-    return 0;
 }
